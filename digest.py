43a8040a923c8bebed91bb00df83e86d6231cedf
adelmofilho <adelmo.aguiar.filho@gmail.com>
2021-04-21 23:43:09 -0300

    correcao digest'

--- digest.py
+++ digest.py
@@ -3,4 +3,4 @@ import pipes
 client = boto3.client("ecr")
 details = client.describe_images(repositoryName='hardin-senado-votacoes', imageIds=[{'imageTag': 'latest'}])
 digest = details["imageDetails"][0]["imageDigest"]
-print("export DIGEST_TWEET=%s" % (pipes.quote(str(digest))))
+print("export DIGEST=%s" % (pipes.quote(str(digest))))
